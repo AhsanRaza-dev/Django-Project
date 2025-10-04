2eb8ee63b5137fb884d1961827208b56e645b590
Ahsan Raza <ahsanaj695@gmail.com>
2025-10-05 01:21:10 +0500

    data pasing throught the render function

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_view(request):
-    return render(request, 'index.html')
+    data ={
+        'title': 'Home Page',
+        'content': 'Welcome to the Home Page',
+        'clist': ['Python', 'Django', 'JavaScript', 'HTML', 'CSS']
+    }
+    return render(request, 'index.html',data)
 
 def aboutUs_view(request):
     return HttpResponse("This is the About Us page.")
